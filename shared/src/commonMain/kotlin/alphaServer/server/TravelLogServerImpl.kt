f872bfae2cf0f6cf6b6b835301d6e50cdce49569
dominikdoric <dominikdoric277@gmail.com>
2023-10-07 23:32:06 +0200

    Added more data to mock database

--- shared/src/commonMain/kotlin/alphaServer/server/TravelLogServerImpl.kt
+++ shared/src/commonMain/kotlin/alphaServer/server/TravelLogServerImpl.kt
@@ -114,7 +114,118 @@ class TravelLogServerImpl : TravelLogServer {
                 weather = "Mild and sunny",
                 budget = "$1600",
                 duration = "5 days"
+            ),
+            Blog(
+                id = 11,
+                title = "Island Paradise in Maldives",
+                content = "The Maldives is a true paradise with its crystal-clear waters and overwater bungalows.",
+                location = "Maldives",
+                dateCreated = "2023-09-20 13:45:00",
+                placeToVisit = "Overwater bungalow, Coral reefs, Male City",
+                weather = "Tropical climate",
+                budget = "$3500",
+                duration = "1 week"
+            ),
+            Blog(
+                id = 12,
+                title = "Camping in Yosemite National Park",
+                content = "Camping in Yosemite offered stunning views of waterfalls and starry skies.",
+                location = "Yosemite National Park, USA",
+                dateCreated = "2023-09-19 10:30:00",
+                placeToVisit = "Yosemite Falls, Half Dome, Glacier Point",
+                weather = "Mountain climate",
+                budget = "$700",
+                duration = "4 days"
+            ),
+            Blog(
+                id = 13,
+                title = "Exploring the Great Wall of China",
+                content = "Walking along the Great Wall provided a sense of awe and history.",
+                location = "Beijing, China",
+                dateCreated = "2023-09-18 11:15:00",
+                placeToVisit = "Great Wall, Forbidden City, Temple of Heaven",
+                weather = "Varied by season",
+                budget = "$1800",
+                duration = "1 week"
+            ),
+            Blog(
+                id = 14,
+                title = "Wildlife Safari in Kenya",
+                content = "Kenya's national parks teem with magnificent wildlife on safari.",
+                location = "Maasai Mara National Reserve, Kenya",
+                dateCreated = "2023-09-17 14:20:00",
+                placeToVisit = "Safari game drives, Maasai villages, Mara River",
+                weather = "Savannah climate",
+                budget = "$3000",
+                duration = "2 weeks"
+            ),
+            Blog(
+                id = 15,
+                title = "Scenic Views in Santorini",
+                content = "Santorini's stunning sunsets and cliffside views are unforgettable.",
+                location = "Santorini, Greece",
+                dateCreated = "2023-09-16 16:10:00",
+                placeToVisit = "Oia, Fira, Amoudi Bay",
+                weather = "Mediterranean climate",
+                budget = "$2500",
+                duration = "1 week"
+            ),
+            Blog(
+                id = 16,
+                title = "Adventure in the Australian Outback",
+                content = "Exploring the vast Australian Outback is an adventure like no other.",
+                location = "Australian Outback",
+                dateCreated = "2023-09-15 09:30:00",
+                placeToVisit = "Uluru, Kakadu National Park, Alice Springs",
+                weather = "Desert climate",
+                budget = "$2800",
+                duration = "2 weeks"
+            ),
+            Blog(
+                id = 17,
+                title = "Historical Journey in Athens",
+                content = "Athens is a treasure trove of ancient history and archaeological wonders.",
+                location = "Athens, Greece",
+                dateCreated = "2023-09-14 12:45:00",
+                placeToVisit = "Acropolis, Parthenon, Plaka District",
+                weather = "Mild and sunny",
+                budget = "$1500",
+                duration = "5 days"
+            ),
+            Blog(
+                id = 18,
+                title = "Trekking in the Himalayas",
+                content = "Trekking in the Himalayas offers breathtaking mountain scenery and spiritual experiences.",
+                location = "Himalayas, Nepal",
+                dateCreated = "2023-09-13 08:00:00",
+                placeToVisit = "Annapurna Base Camp, Everest Base Camp, Kathmandu",
+                weather = "Mountain climate",
+                budget = "$2000",
+                duration = "3 weeks"
+            ),
+            Blog(
+                id = 19,
+                title = "Relaxation in the Malibu Coastline",
+                content = "The Malibu coastline is perfect for relaxation and beachside activities.",
+                location = "Malibu, California, USA",
+                dateCreated = "2023-09-12 14:15:00",
+                placeToVisit = "Zuma Beach, Malibu Pier, Malibu Creek State Park",
+                weather = "Sunny and coastal",
+                budget = "$1200",
+                duration = "1 week"
+            ),
+            Blog(
+                id = 20,
+                title = "Cultural Heritage in Istanbul",
+                content = "Istanbul's rich cultural heritage is a blend of history, architecture, and delicious cuisine.",
+                location = "Istanbul, Turkey",
+                dateCreated = "2023-09-11 11:30:00",
+                placeToVisit = "Hagia Sophia, Blue Mosque, Grand Bazaar",
+                weather = "Mild and Mediterranean",
+                budget = "$1700",
+                duration = "10 days"
             )
+
         )
     }
 }
