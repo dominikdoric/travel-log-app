b1d49e96774c368b541e063c8eaf23d056082da6
dominikdoric <dominikdoric277@gmail.com>
2023-10-19 19:49:20 +0200

    Updated design of details screen

--- shared/src/commonMain/kotlin/presentation/composables/SingleLineText.kt
+++ shared/src/commonMain/kotlin/presentation/composables/SingleLineText.kt
@@ -1,2 +1,16 @@
 package presentation.composables
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun SingleLineText(label: String, value: String) {
+    Row {
+        Text(text = label, fontWeight = FontWeight.Bold)
+        Text(value)
+    }
+}
--- shared/src/commonMain/kotlin/presentation/screens/blogDetailsScreen/composables/BlogDetailsScreenContent.kt
+++ shared/src/commonMain/kotlin/presentation/screens/blogDetailsScreen/composables/BlogDetailsScreenContent.kt
@@ -2,18 +2,23 @@ package presentation.screens.blogDetailsScreen.composables
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import presentation.composables.SingleLineText
 import presentation.model.BlogUi
 
 @Composable
 fun BlogDetailsScreenContent(blog: BlogUi) {
     Column(modifier = Modifier.padding(16.dp)) {
-        Text(blog.id.toString())
-        Text(blog.title)
-        Text(blog.content)
-        Text(blog.budget)
+        Divider(thickness = 4.dp)
+        SingleLineText(label = "Title: ", value = blog.title)
+        SingleLineText(label = "Content: ", value = blog.content)
+        SingleLineText(label = "Location: ", value = blog.location)
+        SingleLineText(label = "Date: ", value = blog.dateCreated)
+        SingleLineText(label = "Place to visit: ", value = blog.placeToVisit)
+        SingleLineText(label = "Weather: ", value = blog.weather)
+        SingleLineText(label = "Duration: ", value = blog.duration)
     }
 }
